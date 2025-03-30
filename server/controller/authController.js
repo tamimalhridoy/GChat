1806dd4d21e799ba07b6bff099046bcb89237994
tamimalhridoy <tamimalhridoy3@gmail.com>
2025-03-30 15:34:13 +0600

    work prossing

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,5 +1,15 @@
+const userSchema = require("../models/userSchema");
+
 const registration = (req, res) => {
-  res.send("Registration Controller");
+  const { fullName, email, password, avatar } = req.body;
 };
 
-module.exports = { registration, };
+const user = new userSchema({
+  fullName,
+  email,
+  password,
+  avatar,
+});
+user.save();
+
+module.exports = registration;
