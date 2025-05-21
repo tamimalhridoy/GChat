fd2aef9b2ae5794cf839f7142baa8d530dcfddc8
tamimalhridoy <tamimalhridoy3@gmail.com>
2025-05-21 23:26:18 +0600

    slove

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -2,7 +2,6 @@ const express = require("express");
 const registration = require("../../controller/authController");
 const router = express.Router();
 
-// router.get("/registration", registration);
 router.get("/registration", registration);
 
 module.exports = router;
